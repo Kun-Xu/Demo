6bac5ecc0f164767964917e4a167306e4958074d
Roger Xu <xukun.xk@gmail.com>
2019-12-01 00:15:23 -0500

    Create a new Demo project

---
+++ GitDemoProject/GitDemoProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitDemoProject.Global" Language="C#" %>
---
+++ GitDemoProject/GitDemoProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitDemoProject.Startup))]
+namespace GitDemoProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
